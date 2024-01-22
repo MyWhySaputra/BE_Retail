15396e8c4a96c694cea64d778b8bee226127bb25
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-01-23 00:29:22 +0700

    fix

--- src/helper/swagger.helper.js
+++ src/helper/swagger.helper.js
@@ -1,6 +1,5 @@
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-const { SwaggerUIBundle } = require("swagger-ui-dist");
 
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
