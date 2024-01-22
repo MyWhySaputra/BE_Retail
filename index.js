d75c98e4fa80e961e425d99c010d0fd87d259a2e
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-01-22 18:30:24 +0700

    22/01/2024

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const router = require('./src/routes/routes')
+const router = require("./src/routes/routes");
 
-const swaggerJsdoc = require('swagger-jsdoc')
-const swaggerUI = require('swagger-ui-express')
-const swaggerDefinition = require('./src/helper/swagger.helper')
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDefinition = require("./src/helper/swagger.helper");
 
-const cors = require('cors')
+const cors = require("cors");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const swaggerSpec = swaggerJsdoc(swaggerDefinition)
+const swaggerSpec = swaggerJsdoc(swaggerDefinition);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const corsOptions = {
-    origin: true, // Gantilah dengan origin aplikasi frontend Anda
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-}
+  origin: true, // Gantilah dengan origin aplikasi frontend Anda
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 app.use(cors(corsOptions));
 
-app.use('/', router)
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+app.use("/", router);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-app.get('/', (req, res) => {
-    //res.render('home.ejs')
-    res.sendFile('./views/home.html')
-})
+const path = require("path");
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "views", "home.html");
+  res.sendFile(filePath);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
